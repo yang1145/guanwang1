c09870473e65840b0fa5c230998ab9cd6d0a0654
yang1145 <13172193298@163.com>
2025-09-25 00:43:22 +0800

    修复了一个部署问题

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -7,6 +8,11 @@ export default defineConfig(({ mode }) => {
   
   return {
     plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
     server: {
       host: '0.0.0.0',
       port: 3000,
